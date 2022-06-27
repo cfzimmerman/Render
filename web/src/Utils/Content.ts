28fd7c352b4ba3206e452d8ee1b29da3f0ed51c2
aharron <aharron@wisc.edu>
2022-06-27 00:17:26 -0500

    clean up content.ts

--- web/src/Utils/Content.ts
+++ web/src/Utils/Content.ts
@@ -1,36 +1,8 @@
-import {
-  generateVideoThumbnails,
-  generateVideoThumbnailViaUrl
-} from '@rajesh896/video-thumbnails-generator';
-import { v4 } from 'uuid';
+import { generateVideoThumbnailViaUrl } from '@rajesh896/video-thumbnails-generator';
 
-// const fs = require('browserify-fs');
-
-// export const saveToBrowserFs = (file: string) => {
-//   fs.mkdir('/files', () => {
-//     fs.writeFile('/files/thumbnail.jpeg', 'Hello world!\n', () => {
-//       fs.readFile('/home/hello-world.txt', 'utf-8', (err, data) => {
-//         console.log(data);
-//       });
-//     });
-//   });
-// };
-
-export const blobToImage = (blob: string): File =>
-  // const byteCharacters = atob(blob);
-  // const byteNumbers = new Array(byteCharacters.length);
-  // // eslint-disable-next-line no-plusplus
-  // for (let i = 0; i < byteCharacters.length; i++) {
-  //   byteNumbers[i] = byteCharacters.charCodeAt(i);
-  // }
-  // const byteArray = new Uint8Array(byteNumbers);
-  new File([blob], 'thumbnail1.png', { type: 'image/png' });
 export const generateVideoThumbnail = async (file: File): Promise<string> => {
-  const array = await generateVideoThumbnails(file, 0, '');
   const arr = await generateVideoThumbnailViaUrl(URL.createObjectURL(file), 0);
   return arr;
-  // console.log(arr);
-  // return blobToImage(arr);
 };
 
 export const dataURItoBlob = (data: string) => {
