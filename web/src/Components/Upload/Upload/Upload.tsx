b0748158d5472bcf77348ca5e63c1b47ae4d420f
aharron <38299610+aharron@users.noreply.github.com>
2022-07-18 22:54:36 -0500

    add drag and drop file upload (#67)

--- web/src/Components/Upload/Upload/Upload.tsx
+++ web/src/Components/Upload/Upload/Upload.tsx
@@ -1,18 +1,28 @@
-import React, { ChangeEvent, useRef } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useCallback, useMemo, useRef } from 'react';
+import { useDropzone } from 'react-dropzone';
 import styles from './Upload.module.css';
 
 interface Props {
   setFiles: (files: File[]) => void;
 }
 
+const uploadAcceptStyle = {
+  borderColor: 'var(--success-green)'
+};
+
+const uploadRejectStyle = {
+  borderColor: 'var(--reject-red)'
+};
+
 export const Upload: React.FC<Props> = (props) => {
   const { setFiles } = props;
 
   const fileInput = useRef<HTMLInputElement>(null);
 
-  const handleUpload = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      const sorted = Array.from(event.target.files).sort((a, b) => {
+  const handleUpload = useCallback(
+    (files: File[]) => {
+      const sorted = files.sort((a, b) => {
         if (a.type.includes('image') && b.type.includes('video')) {
           return -1;
         }
@@ -22,8 +32,21 @@ export const Upload: React.FC<Props> = (props) => {
         return 0;
       });
       setFiles(sorted);
-    }
-  };
+    },
+    [setFiles]
+  );
+
+  const onDrop = useCallback(
+    (files: File[]) => {
+      if (!!files && !!files.length) handleUpload(files);
+    },
+    [handleUpload]
+  );
+
+  const { isDragAccept, isDragReject, getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: { 'image/*': [], 'video/*': [] }
+  });
 
   const handleClickUpload = () => {
     if (fileInput.current) {
@@ -31,19 +54,33 @@ export const Upload: React.FC<Props> = (props) => {
     }
   };
 
+  const uploadStyle = useMemo(
+    () => ({
+      ...(isDragAccept ? uploadAcceptStyle : {}),
+      ...(isDragReject ? uploadRejectStyle : {})
+    }),
+    [isDragAccept, isDragReject]
+  );
+
   return (
     <div className={styles.content}>
-      <input
-        type="file"
-        ref={fileInput}
-        onChange={handleUpload}
-        accept="image/*,video/*"
-        multiple
-      />
-      <button className={styles.uploadButton} type="button" onClick={handleClickUpload}>
-        {/* <h3>Drag and drop or</h3> */}
-        <h3>Click here to upload</h3>
-      </button>
+      <div {...getRootProps({ className: styles.contentBox, style: uploadStyle })}>
+        <input
+          ref={fileInput}
+          {...getInputProps({
+            type: 'file',
+            onChange: (event) => {
+              if (event.target.files) handleUpload(Array.from(event.target.files));
+            },
+            accept: 'image/*,video/*',
+            multiple: true
+          })}
+        />
+        <button className={styles.uploadButton} type="button" onClick={handleClickUpload}>
+          <h3>Drag and drop or</h3>
+          <h3>Click here to upload</h3>
+        </button>
+      </div>
     </div>
   );
 };
