22da2036808a4f12cd2b994d4cab1c5eaa9363d6
rendergame <admin@render.game>
2022-07-27 10:55:52 -0500

    Multi select continued

--- mobile/src/screens/tabnav/vault/VaultSectionItem.tsx
+++ mobile/src/screens/tabnav/vault/VaultSectionItem.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { Image, TouchableOpacity, StyleSheet } from "react-native";
-import { GlobalStyles, Environment } from "../../../resources/project";
+import { Image, TouchableOpacity, StyleSheet, View } from "react-native";
+import {
+  GlobalStyles,
+  Environment,
+  Colors,
+  Icons,
+} from "../../../resources/project";
 import ExternalVaultTileInfo from "./ExternalVaultTileInfo";
 import TransitionToFullView from "./TransitionToFullView";
 
@@ -14,6 +19,7 @@ const CorrectURI = ({ item }) => {
 };
 
 const AreEqual = (previousProps, nextProps) => {
+  // rerender if array id or item refrernces the currrent item
   if (
     previousProps.item.contentkey === nextProps.item.contentkey &&
     previousProps.item.publicpost === nextProps.item.publicpost &&
@@ -28,20 +34,47 @@ const VaultSectionItem = ({ item, navigation, vaultfeeddata }) => {
   // SectionItem is getting a version of vaultfeeddata with one fewer items than needed, pushing transitiontofullview to spit out value not found and navigating to the beginning of the array
   const contenturl = CorrectURI({ item });
 
+  const multiSelectActive = false;
+  var isSelected = false;
+
+  const ShortPressAction = () => {
+    if (multiSelectActive === false) {
+      TransitionToFullView({
+        id: item.id,
+        navigation,
+        data: vaultfeeddata,
+        usecase: "vault",
+      });
+    } else {
+      if (isSelected === true) {
+        isSelected = false;
+      } else {
+        isSelected = true;
+      }
+    }
+  };
+
   return (
     <TouchableOpacity
       style={[GlobalStyles.shadow, styles.touchablecontainer]}
-      onPress={() => {
-        TransitionToFullView({
-          id: item.id,
-          navigation,
-          data: vaultfeeddata,
-          usecase: "vault",
-        });
-      }}
+      onPress={ShortPressAction}
+      onLongPress={() => (isSelected = true)}
     >
       <Image style={styles.previewimage} source={{ uri: contenturl }} />
       <ExternalVaultTileInfo item={item} origin="sectionitem" />
+      <View
+        style={[
+          styles.checkedTileBackground,
+          { opacity: isSelected ? 0.5 : 0 },
+        ]}
+      >
+        <Icons.OriginalSize.Checkmark
+          style={{ opacity: 1 }}
+          stroke={Colors.Primary}
+          height={3 * Environment.IconSize}
+          width={3 * Environment.IconSize}
+        />
+      </View>
     </TouchableOpacity>
   );
 };
@@ -55,6 +88,15 @@ const styles = StyleSheet.create({
   touchablecontainer: {
     alignItems: "flex-end",
     justifyContent: "flex-end",
+  },
+  checkedTileBackground: {
+    backgroundColor: Colors.AccentOn,
+    height: Environment.HalfBar,
+    width: Environment.HalfBar,
+    position: "absolute",
+    borderRadius: Environment.StandardRadius,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
