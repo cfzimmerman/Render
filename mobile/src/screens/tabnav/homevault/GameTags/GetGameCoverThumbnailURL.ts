4160e09f08274849a492e73a025985cd937c060a
rendergame <82123491+rendergame@users.noreply.github.com>
2022-08-23 11:23:35 -0500

    Fix delete post, remove tag bug (#90)

--- mobile/src/screens/tabnav/homevault/GameTags/GetGameCoverThumbnailURL.ts
+++ mobile/src/screens/tabnav/homevault/GameTags/GetGameCoverThumbnailURL.ts
@@ -1,6 +1,9 @@
-const GetGameCoverThumbnailURL = ({ coverID }: { coverID: string }) => {
-  const coverThumbnailURL = `https://images.igdb.com/igdb/image/upload/t_cover_small/${coverID}.png`;
-  return coverThumbnailURL;
+const GetGameCoverThumbnailURL = ({ coverID }: { coverID: string | null }) => {
+  if (typeof coverID === "string") {
+    const coverThumbnailURL = `https://images.igdb.com/igdb/image/upload/t_cover_small/${coverID}.png`;
+    return coverThumbnailURL;
+  }
+  return null;
 };
 
 export default GetGameCoverThumbnailURL;
