5ce64e7bb561f00ed1a5d870fcb38d5872eb89ad
rendergame <admin@render.game>
2022-11-04 21:43:16 -0500

    Polish off files that survived the purge

--- mobile/src/screens/masterstack/AppStart.ts
+++
@@ -1,9 +0,0 @@
-import { ObserveInboundLink } from "../../resources/utilities";
-import GetCurrentUser from "../tabnav/profile/GetCurrentUser";
-
-const AppStart = ({ dispatch, navigation }) => {
-  GetCurrentUser({ dispatch, navigation });
-  ObserveInboundLink({ dispatch, navigation });
-};
-
-export default AppStart;
